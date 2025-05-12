94c7c856c666514209eb361dc0b787ad93283a58
Shikhar <shikher.2003@gmail.com>
2025-05-12 23:57:34 +0530

    fix lint issues

--- lib/neo4j.ts
+++ lib/neo4j.ts
@@ -1,25 +1,36 @@
-import neo4j, { Driver } from 'neo4j-driver'
+import neo4j, { Driver } from "neo4j-driver";
 
-let driver: Driver
+let driver: Driver;
 
 declare global {
-  var neo4j: Driver | undefined
+  interface Global {
+    _neo4jDriver?: Driver;
+  }
+
+  // This is necessary in TypeScript to extend globalThis properly
+  namespace NodeJS {
+    interface Global {
+      _neo4jDriver?: Driver;
+    }
+  }
 }
 
-if (process.env.NODE_ENV === 'production') {
+// Use globalThis to store the singleton
+const globalRef = globalThis as typeof globalThis & { _neo4jDriver?: Driver };
+
+if (process.env.NODE_ENV === "production") {
   driver = neo4j.driver(
     process.env.NEO4J_URI!,
     neo4j.auth.basic(process.env.NEO4J_USER!, process.env.NEO4J_PASSWORD!)
-  )
+  );
 } else {
-  if (!global.neo4j) {
-    global.neo4j = neo4j.driver(
+  if (!globalRef._neo4jDriver) {
+    globalRef._neo4jDriver = neo4j.driver(
       process.env.NEO4J_URI!,
       neo4j.auth.basic(process.env.NEO4J_USER!, process.env.NEO4J_PASSWORD!)
-    )
+    );
   }
-  driver = global.neo4j
+  driver = globalRef._neo4jDriver;
 }
 
-export { driver }
-
+export { driver };
